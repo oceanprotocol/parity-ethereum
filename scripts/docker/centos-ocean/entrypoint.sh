c4bbb18e30b91aeac248a273d62b41d3fd36f897
Javier Cortejoso <javier.cortejoso@gmail.com>
2018-12-24 12:52:57 +0100

    Fix entrypoint

--- scripts/docker/centos-ocean/entrypoint.sh
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -5,8 +5,8 @@ if [ "${CONFIGURE_ACL}" = "true" ]; then
   while [ ! -f "/contracts/ready" ]; do
     sleep 2
   done
-  acl_contract=$(cat /contracts/AccessConditions.json | jq -r .address)
-  sed -i -e "/acl_contract = \"none\"/acl_contract = \"${acl_contract}\"" /etc/parity/secretstore/config.toml
+  acl_contract=$(cat /contracts/AccessConditions.*.json | jq -r .address)
+  sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
 fi
 
 /opt/parity/parity "$@"
