bae7195d4570a91f7fa267e4b065da239709a62d
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-25 18:20:55 +0100

    Added debug info

--- scripts/docker/centos-ocean/entrypoint.sh
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -6,6 +6,7 @@ if [ "${CONFIGURE_ACL}" = "true" ]; then
     sleep 2
   done
   acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+  echo "ACL Contract Address: ${acl_contract}"
   sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
 else
   sed -i -e "/acl_contract = .*/c acl_contract = \"none\"" /etc/parity/secretstore/config.toml
