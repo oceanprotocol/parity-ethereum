64fc0454bddf12f890d8e4e0568b932959489b98
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-25 15:38:57 +0100

    Updated with Ocean docker files

---
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "${CONFIGURE_ACL}" = "true" ]; then
+  echo "Waiting for contracts to be generated..."
+  while [ ! -f "/contracts/ready" ] || [ -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
+    sleep 2
+  done
+  acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+  sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
+else
+  sed -i -e "/acl_contract = .*/c acl_contract = \"none\"" /etc/parity/secretstore/config.toml
+fi
+
+/opt/parity/parity "$@"
