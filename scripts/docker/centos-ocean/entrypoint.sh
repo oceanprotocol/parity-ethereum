da18547fe4f2511b9e368eb9241ce12c3f686223
Javier Cortejoso <javier.cortejoso@gmail.com>
2019-01-28 11:27:12 +0100

    Added variable ACL_CONTRACT_ADDRESS

--- scripts/docker/centos-ocean/entrypoint.sh
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 if [ "${CONFIGURE_ACL}" = "true" ]; then
-  echo "Waiting for contracts to be generated..."
-  while [ ! -f "/contracts/ready" ] || [ -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
-    sleep 2
-  done
-  acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+  if [ ! -z "${ACL_CONTRACT_ADDRESS}" ]; then
+    acl_contract="${ACL_CONTRACT_ADDRESS}"
+  else
+    echo "Waiting for contracts to be generated..."
+    while [ ! -f "/contracts/ready" ] || [ -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
+      sleep 2
+    done
+    acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+  fi
   echo "ACL Contract Address: ${acl_contract}"
   sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
 else
