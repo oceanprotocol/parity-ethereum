ad957011d5a9e2af873e9b9226f2bd6f6ad1c341
Aitor <aaitor@gmail.com>
2019-02-28 17:24:48 +0100

    Adapting name of Acl Contract because keeperv08

--- scripts/docker/centos-ocean/entrypoint.sh
+++ scripts/docker/centos-ocean/entrypoint.sh
@@ -5,10 +5,10 @@ if [ "${CONFIGURE_ACL}" = "true" ]; then
     acl_contract="${ACL_CONTRACT_ADDRESS}"
   else
     echo "Waiting for contracts to be generated..."
-    while [ ! -f "/contracts/ready" ] || [ ! -f "/contracts/AccessConditions.${NETWORK}.json" ]; do
+    while [ ! -f "/contracts/ready" ] || [ ! -f "/contracts/AccessSecretStoreCondition.${NETWORK}.json" ]; do
       sleep 2
     done
-    acl_contract=$(cat /contracts/AccessConditions.${NETWORK}.json | jq -r .address)
+    acl_contract=$(cat /contracts/AccessSecretStoreCondition.${NETWORK}.json | jq -r .address)
   fi
   echo "ACL Contract Address: ${acl_contract}"
   sed -i -e "/acl_contract = .*/c acl_contract = \"${acl_contract:2}\"" /etc/parity/secretstore/config.toml
